cc21d151c96e016d341468f5772bf1cc48308ad3
alualualu <preetheparama@gmail.com>
2017-06-20 17:06:38 +0530

    Create countint.java

---
+++ countint.java
@@ -0,0 +1,24 @@
+package pro1;
+
+import java.util.Scanner;
+
+
+public class Pro1 {
+
+   
+    public static void main(String[] args) {
+       
+            {
+              
+
+        int n;
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        n = s.nextInt();
+        int len=(int)Math.log10(n)+1;
+        System.out.println("the lenghth is:"+len);
+        }
+        }
+        }
+         
