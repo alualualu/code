52444f4f4b88d99a603eea5bb7c03056c651e0a0
alualualu <preetheparama@gmail.com>
2017-06-20 16:47:37 +0530

    Create natusum.java

---
+++ natusum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Pro1 {
+
+    
+    public static void main(String[] args) {
+       
+            {
+              
+
+        int n,a,b,c,d,ans;
+
+        Scanner s = new Scanner(System.in);
+
+       System.out.print("Enter the 1st number:");
+        a = s.nextInt();
+         System.out.print("Enter the 2st number:");
+        b = s.nextInt();
+         System.out.print("Enter the 3st number:");
+        c = s.nextInt();
+         System.out.print("Enter the 4st number:");
+        d = s.nextInt();
+        ans=a+b+c+d;
+        System.out.print("Answer is:"+ans);
+        }
