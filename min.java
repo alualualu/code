5ae7be9a4fdc29cec8ee68870d792bb99d19ad93
alualualu <preetheparama@gmail.com>
2017-06-22 16:20:43 +0530

    Create min.java

---
+++ min.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s =new Scanner(System.in);
+        System.out.println("enter the 2 numbers (h,k)to find");
+        int h = s.nextInt();
+        int k=s.nextInt();
+        if(h>k)
+        {
+            System.out.println("k is minimum");
+        }
+        if(k>h)
+        {
+            System.out.println("h is minimum");
+        }
+    }
+        }
+        
+        
