6c07796d5486f5076cfa1cf1edb5a2025e918b80
alualualu <preetheparama@gmail.com>
2017-06-22 09:44:45 +0530

    Create revstring.java

---
+++ revstring.java
@@ -0,0 +1,6 @@
+ public static void main(String[] args) {
+      String string = "hello";
+      String reverse = new StringBuffer(string).reverse().toString();
+      System.out.println("\nString before reverse: "+string);
+      System.out.println("String after reverse: "+reverse);
+   }
