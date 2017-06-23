b05ef4a2591cd1b0f68ccd78bf48f93a6ed57a4e
alualualu <preetheparama@gmail.com>
2017-06-23 15:06:46 +0530

    Create multiply.java

---
+++ multiply.java
@@ -0,0 +1,13 @@
+ public static void main(String[] args) {
+        
+        // TODO code application logic here
+        Scanner s= new Scanner(System.in);
+        System.out.println("enter the table number");
+        int t=s.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+        System.out.println(+t+"*"+i+"="+(t*i));
+    }
+    }
+    
+}
