fdfb34c0d775ad4eaae277c2b924c051a03f7b85
alualualu <preetheparama@gmail.com>
2017-06-23 15:59:10 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,13 @@
+ public static void main(String[] args) {
+        
+        // TODO code application logic here
+       
+         Scanner s= new Scanner(System.in);
+        System.out.println("enter the 1st number");
+        int i=s.nextInt();
+       
+        System.out.println("enter the 2st number");
+        int j=s.nextInt();
+       System.out.println(Math.pow(i,j));
+    }
+    
