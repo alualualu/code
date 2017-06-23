949449eb7f0b2063936db899b7adc848c60d2c6a
alualualu <preetheparama@gmail.com>
2017-06-23 12:26:54 +0530

    Create oddlist.java

---
+++ oddlist.java
@@ -0,0 +1,13 @@
+ public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s= new Scanner(System.in);
+        System.out.println("enter the limit ");
+       int  n = s. nextInt();
+       for(int i=1; i<=n; i++)
+       {
+           if(i%2==1)
+               System.out.println("odd sereies"+"  "+ i);
+                  
+       }
+           
+        
